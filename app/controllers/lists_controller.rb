5bb2ea660926189be62df10ab554533bbf08a1d9
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-09-04 08:33:42 +0000

    [Finish]application1-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,9 +5,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -21,7 +24,7 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'lists/new'
   resources :lists
-  
+
 end
